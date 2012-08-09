f7f810112643f6022db56ba0968791fc308d7f0e
Yossi Shalem <yossishalem@gmail.com>
2012-08-10 01:14:06 +0300

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedFiction::Application.config.secret_token = '118cdecd550980415b4cb0eeb118570d5427ee55c2b3ed3d24e9c99a0d1e711e6ad7b0c0ba74adb8b1dbf823a8557dab4dfe8587b429b9c444d3a462e4456f9c'
