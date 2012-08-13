c003458f04c7386873e3bdf5db8fe26ee7bdb808
Yossi Shalem <yossishalem@gmail.com>
2012-08-14 01:52:06 +0300

    fix js and added debug for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,6 @@ FeedFiction::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  ENV['FACEBOOK_APP_ID'] = '244160649037860'
+  ENV['FACEBOOK_SECRET'] = 'c7974ab01e76c15d62823eca67f51d2d'
 end
