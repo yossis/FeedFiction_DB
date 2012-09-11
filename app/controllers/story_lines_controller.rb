eb1c99eaf0957b48b991a4690256478b2628871d
yossis <yossishalem@gmail.com>
2012-09-12 01:36:53 +0300

    fix comments

--- app/controllers/story_lines_controller.rb
+++ app/controllers/story_lines_controller.rb
@@ -48,7 +48,7 @@ class StoryLinesController < ApplicationController
 
     #logger.debug "last_line: #{params[:story_line].attributes.inspect}"
     #logger.debug "Post should be valid: #{@post.valid?}"
-    debugger
+    
     if (last_line.order_id==params[:story_line][:order_id].to_i)
         @story_line.order_id +=1
         @story_line.save!
