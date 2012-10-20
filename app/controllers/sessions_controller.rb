81716b6dc3834610af39ae86e409088e2305435e
Yossi Shalem <yossishalem@gmail.com>
2012-10-20 20:52:05 +0200

    fixing welcome email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user.first_time?
       start_wizard
       redirect_to start_story_wizard_url
-      UserMailer.welcome(@user).deliver if user.email.present?
+      UserMailer.welcome(user).deliver if user.email.present?
     else
       redirect_to root_url
     end
