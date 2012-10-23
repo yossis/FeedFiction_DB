e3c89c7b2b98ca22a7488e70359a0efb54633d69
yossis <yossishalem@gmail.com>
2012-10-23 08:52:29 +0200

    added validation in acounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,8 @@
 class Account < ActiveRecord::Base
   attr_accessible :enable, :name, :password, :username
+  validates :username, presence: true
+  validates_uniqueness_of :username
+  validates :password , presence: true
 
   def self.authorize(usr,pwd)
   	u = where(username: usr, password: pwd, enable: true).first
