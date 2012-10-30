8a49038360d780c8d3503022ca27c49c8e9d8c3f
Yossi Shalem <yossishalem@gmail.com>
2012-10-30 23:58:28 +0200

    fixed following button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,9 @@
 class RelationshipsController < ApplicationController
-  #before_filter :signed_in_user
+  before_filter :set_common_vars
 
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    logger.info @user.name
     respond_to do |format|
       format.html { redirect_to @user }
       format.js { render template: 'users/show_header' }
@@ -19,5 +18,9 @@ class RelationshipsController < ApplicationController
       format.html { redirect_to @user }
       format.js { render template: 'users/show_header' }
     end
+  end
+
+  def set_common_vars
+     @in_header = true if params[:is_header]=='true'
   end
 end
