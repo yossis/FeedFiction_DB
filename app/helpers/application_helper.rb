baf54f9028bacdcf83354b25d0c5a1c6d4c03034
Yossi Shalem <yossishalem@gmail.com>
2012-11-11 01:11:02 +0200

    continue styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,29 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
-	  def full_title(page_title)
-	    base_title = "Feed Fiction :: Photos are creating Stories"
-	    if page_title.present?
-	    	"#{page_title} - FeedFiction.com"
-	    else
-	      base_title
-	    end
-	  end
+		def full_title(page_title)
+			base_title = "Feed Fiction :: Photos are creating Stories"
+			if page_title.present?
+				"#{page_title} - FeedFiction.com"
+			else
+				base_title
+			end
+		end
 
-	  def number_to_pixel(num ={})
-	  	num ||= "365"
-	  	"#{num}px"
-	  end
+		def number_to_pixel(num ={})
+			num ||= "365"
+			"#{num}px"
+		end
 
-	  def drop_down_categories
-	  	@categories = Category.all
+		def drop_down_categories
+			@categories = Category.all
 		render 'categories/categories_drop_down' 
-	  end
-	  
+		end
+
+		def drop_cap(line)
+			line.capitalize!
+			#line.gsub!(line[0],'<span class="dropcap">'+line[0]+'</span>')
+			line.insert(1,'</span>').insert(0,'<span class="dropcap">')
+			line.html_safe
+			#'line[0]='+line[0]+', other='+line
+		end
 end
