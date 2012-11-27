98777ab944df3c1bc1601b13470d438af93384be
yossis <yossishalem@gmail.com>
2012-11-27 16:02:13 +0200

    followin pages

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -8,19 +8,5 @@ module ImagesHelper
 		image.image_source
 	end
 
-	def avatar_rounded(user)
-		# img = '<div class="round-avatar"><div></div>'+ link_to(image_tag(user.avatar) ,user, title: user.name)
-		# img +='</div>'
-		img = '<div class="round-avatar">'+ link_to(image_tag(user.avatar, class: 'img-circle') ,user, title: user.name)
-		img +='</div>'
-		img.html_safe
-	end
-
-	def avatar_rounded_large(user)
-		# img = '<div class="round-avatar"><div></div>'+ link_to(image_tag(user.avatar) ,user, title: user.name)
-		# img +='</div>'
-		img = '<div class="round-avatar-large">'+ link_to(image_tag(user.avatar, size: "90x90", class: 'img-circle' ) ,user, title: user.name)
-		img +='</div>'
-		img.html_safe
-	end
+	
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,30 @@ module UsersHelper
 		link_to image_tag(user.avatar,:class => 'border-image-small') ,user, :title => user.name
 	end
 
+	def avatar_rounded(user)
+		# img = '<div class="round-avatar"><div></div>'+ link_to(image_tag(user.avatar) ,user, title: user.name)
+		# img +='</div>'
+		img = '<div class="round-avatar">'+ link_to(image_tag(user.avatar, class: 'img-circle') ,user, title: user.name)
+		img +='</div>'
+		img.html_safe
+	end
+
+	def avatar_rounded_large(user)
+		# img = '<div class="round-avatar"><div></div>'+ link_to(image_tag(user.avatar) ,user, title: user.name)
+		# img +='</div>'
+		img = '<div class="round-avatar-large">'+ link_to(image_tag(user.avatar, size: "90x90", class: 'img-circle' ) ,user, title: user.name)
+		img +='</div>'
+		img.html_safe
+	end
+
+	def avatar_rounded_medium(user)
+		# img = '<div class="round-avatar"><div></div>'+ link_to(image_tag(user.avatar) ,user, title: user.name)
+		# img +='</div>'
+		img = '<div class="round-avatar-medium">'+ link_to(image_tag(user.avatar, size: "60x60", class: 'img-circle' ) ,user, title: user.name)
+		img +='</div>'
+		img.html_safe
+	end
+
 	def user_link(user, options={})
 		link_to(user.name , user , options.merge(:title => user.name))
 	end
