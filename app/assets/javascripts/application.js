3c0b9d7275bded29b1f085769e60edf0e2bffd86
Yossi Shalem <yossishalem@gmail.com>
2012-12-06 20:50:39 +0200

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 // require bootstrap
-//= require jquery-fileupload/basic
-//= require jquery-fileupload/vendor/tmpl
+// require jquery-fileupload/basic
+// require jquery-fileupload/vendor/tmpl
 //= require_tree .
