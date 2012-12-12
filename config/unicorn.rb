a1884b31cf0b38224c992d62b0a6d95d3f8dd789
Yossi Shalem <yossishalem@gmail.com>
2012-12-12 23:10:19 +0200

    configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -80,7 +80,10 @@ after_fork do |server, worker|
   # Redis and Memcached would go here but their connections are established
   # on demand, so the master never opens a socket
 
-
+  # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => 'redis://localhost:6379/12'}
+  end
   ##
   # Unicorn master is started as root, which is fine, but let's
   # drop the workers to git:git
