985e6d38a2c20356552e992398269f9ec865454e
yossis <yossishalem@gmail.com>
2013-01-07 12:14:46 +0200

    added ui

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,10 +2,10 @@ module ImagesHelper
 	def image_thumb(image)
 		if image.image_processed?
 			url = image.image_thumb_url(:thumb)
-			image_tag(url, height: image.height, width: image.width)
+			image_tag(url, height: image.height, width: image.width, style: "height:#{image.height}px;width:#{image.width}")
 			
 		else
-			image_tag(image.image_source, height: image.height, width: image.width)
+			image_tag(image.image_source, height: image.height, width: image.width , style: "height:#{image.height}px;width:#{image.width}")
 		end
 	end
 end
